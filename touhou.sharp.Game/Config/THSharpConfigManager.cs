7640fabd4dd9e79c97d77c605986d49097cced98
Shawdooow <banhammer1915@gmail.com>
2018-06-01 22:25:37 -0400

    add basic config manager for gamdmodes

---
+++ touhou.sharp.Game/Config/THSharpConfigManager.cs
@@ -0,0 +1,49 @@
+﻿using osu.Framework.Configuration;
+using osu.Framework.Platform;
+
+namespace touhou.sharp.Game.Config
+{
+    public class THSharpConfigManager : IniConfigManager<THSharpSetting>
+    {
+        protected override string Filename => @"THSharp.ini";
+
+        public THSharpConfigManager(Storage storage) : base(storage)
+        {
+        }
+
+        protected override void InitialiseDefaults()
+        {
+            Set(THSharpSetting.Gamemode, Gamemodes.Touhou);
+
+            Set(THSharpSetting.SavedName, "User");
+            Set(THSharpSetting.SavedUserID, -1);
+            Set(THSharpSetting.PlayerColor, "#ffffff");
+
+            Set(THSharpSetting.HostIP, "Host's IP Address");
+            Set(THSharpSetting.LocalIP, "Local IP Address");
+
+            Set(THSharpSetting.HostPort, 25570);
+            Set(THSharpSetting.LocalPort, 25570);
+        }
+    }
+
+    public enum THSharpSetting
+    {
+        Gamemode,
+
+        SavedName,
+        SavedUserID,
+        PlayerColor,
+
+        HostIP,
+        LocalIP,
+
+        HostPort,
+        LocalPort
+    }
+
+    public enum Gamemodes
+    {
+        Touhou
+    }
+}
--- touhou.sharp.Game/THSharpBaseGame.cs
+++ touhou.sharp.Game/THSharpBaseGame.cs
@@ -1,9 +1,13 @@
 ﻿using osu.Framework.Allocation;
+using osu.Framework.Platform;
+using touhou.sharp.Game.Config;
 
 namespace touhou.sharp.Game
 {
     public class THSharpBaseGame : osu.Framework.Game
     {
+        protected THSharpConfigManager THSharpConfigManager;
+
         protected override string MainResourceFile => "touhou.sharp.Game.Resources.dll";
 
         private DependencyContainer dependencies;
@@ -11,10 +15,30 @@ namespace touhou.sharp.Game
         protected override IReadOnlyDependencyContainer CreateLocalDependencies(IReadOnlyDependencyContainer parent) =>
             dependencies = new DependencyContainer(base.CreateLocalDependencies(parent));
 
+        public THSharpBaseGame()
+        {
+            Name = @"TouhouSharp";
+        }
+
         [BackgroundDependencyLoader]
         private void load()
         {
             dependencies.Cache(this);
+            dependencies.Cache(THSharpConfigManager);
+        }
+
+        public override void SetHost(GameHost host)
+        {
+            if (THSharpConfigManager == null)
+                THSharpConfigManager = new THSharpConfigManager(host.Storage);
+
+            base.SetHost(host);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            THSharpConfigManager?.Save();
+            base.Dispose(isDisposing);
         }
     }
 }
